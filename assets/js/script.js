fbf4d0e36ac8a0e250006403025ac8e153111e7b
sofiaBarbaday <sofi.barbaday@gmail.com>
2025-11-17 03:05:46 -0300

    agrego script del TP7

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,24 @@
-// --- Seleccionar elementos del DOM ---
+// Seleccionar elementos del DOM
 const botonModal = document.getElementById("btn-modal");
 const modal = document.getElementById("modal");
 const cerrar = document.getElementById("cerrar-modal");
 
-// --- Evento: click izquierdo ---
+//click izquierdo=mostrar modal
 botonModal.addEventListener("click", () => {
     modal.classList.remove("oculto");
-    modal.classList.add("mostrar", "animar");
+    modal.classList.add("mostrar");
 });
 
-// --- Evento: click derecho ---
+//click derecho=mostrar modal (evitando menú)
 botonModal.addEventListener("contextmenu", (e) => {
     e.preventDefault(); // evita menú del clic derecho
     modal.classList.remove("oculto");
-    modal.classList.add("mostrar", "animar", "resaltar");
+    modal.classList.add("mostrar");
 });
 
-// --- Cerrar modal ---
+// Cerrar modal
 cerrar.addEventListener("click", () => {
-    modal.classList.remove("mostrar", "resaltar", "animar");
+    modal.classList.remove("mostrar");
     modal.classList.add("oculto");
 });
+
